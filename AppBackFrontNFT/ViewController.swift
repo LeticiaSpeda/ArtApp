1b40661cd3a17c838dc78aac08f8d13634f360fd
Leticia Speda <leticiaspeda@gmail.com>
2023-11-29 11:11:41 -0300

    feat: adicionado imagem do logo e criando os componentens na tela de viewController

--- AppBackFrontNFT/ViewController.swift
+++ AppBackFrontNFT/ViewController.swift
@@ -1,10 +1,28 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, ViewCode {
+
+    private lazy var verticalStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 15
+        stack.enableViewCode()
+        return stack
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
+
+    func setupHierarchy() {
+
+    }
+
+    func setupConstraints() {
+
+    }
 }
+
 
