e5c522fff424c08414ead8eda5d5a22743dbd10b
Leticia Speda <leticiaspeda@gmail.com>
2023-12-01 11:00:11 -0300

    feat: adicionado funcao para backroud da tela ser gradient: Description: - retirado a imagem de fundo - adicionado ao emailTextField configuracao para pintar o placeholder e o texto na hora da digitacao - adicionado ao emailTextField configuracao para dar espacamento da mensagem do placeholder da borda

--- AppBackFrontNFT/Aplication/SceneDelegate.swift
+++ AppBackFrontNFT/Aplication/SceneDelegate.swift
@@ -4,12 +4,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = .init(windowScene: windowScene)
         window?.rootViewController = LoginViewController()
         window?.makeKeyAndVisible()
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- AppBackFrontNFT/LoginViewController.swift
+++ AppBackFrontNFT/LoginViewController.swift
@@ -10,13 +10,6 @@ final class LoginViewController: UIViewController, ViewCode {
         return stack
     }()
 
-    private lazy var imageBottom: UIImageView = {
-        let image = UIImageView(image: UIImage.gradient)
-        image.contentMode = .scaleAspectFit
-        image.enableViewCode()
-        return image
-    }()
-
     private lazy var imageLogo: UIImageView = {
         let image = UIImageView(image: UIImage.logo)
         image.contentMode = .scaleAspectFit
@@ -24,48 +17,112 @@ final class LoginViewController: UIViewController, ViewCode {
         return image
     }()
 
+    private lazy var titleApp: UILabel = {
+        let label = UILabel()
+        label.text = "BackFront NFT"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var subTitleApp: UILabel = {
+        let label = UILabel()
+        label.text = "O marketplace da NFT da Backfront Academy"
+        label.textColor = .white.withAlphaComponent(0.5)
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .white
+        textField.tintColor = .white
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.layer.cornerRadius = 10
+        textField.keyboardType = .emailAddress
+        textField.attributedPlaceholder = NSAttributedString(string: "usuario@email.com", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white.withAlphaComponent(0.6)])
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 62))
+        textField.leftViewMode = .always
+        textField.enableViewCode()
+        return textField
+    }()
+
+
+    func setGradientBackground() {
+        let colorTop =  UIColor(
+            red: 36.0/255.0,
+            green: 36.0/255.0,
+            blue: 36.0/255.0,
+            alpha: 1.0
+        ).cgColor
+        let colormiddle = UIColor(
+            red: 82.0/255.0,
+            green: 04.0/255.0,
+            blue: 92.0/255.0,
+            alpha: 0.0
+        ).cgColor
+        let colorBottom = UIColor(
+            red: 42.0/255.0,
+            green: 41.0/255.0,
+            blue: 41.0/255.0,
+            alpha: 1.0
+        ).cgColor
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colormiddle, colorBottom]
+        gradientLayer.locations = [0.0, 1.0, 0.0]
+        gradientLayer.frame = self.view.bounds
+
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         commonInit()
     }
 
     func setupHierarchy() {
-        view.addSubview(imageBottom)
+        view.addSubview(verticalStackView)
 
-        imageBottom.addSubview(verticalStackView)
-
+        verticalStackView.addArrangedSubview(UIView())
         verticalStackView.addArrangedSubview(imageLogo)
+        verticalStackView.addArrangedSubview(titleApp)
+        verticalStackView.addArrangedSubview(subTitleApp)
+        verticalStackView.addArrangedSubview(emailTextField)
         verticalStackView.addArrangedSubview(UIView())
     }
 
     func setupConstraints() {
         NSLayoutConstraint.activate(
-            setupImageBottomContraints() +
             setupVerticalStackConstraints() +
-            setupImageLogoConstraints()
+            setupImageLogoConstraints() +
+            setupEmailTextFieldConstraints()
         )
-    }
-
-    private func setupImageBottomContraints() -> [NSLayoutConstraint] {
-        [imageBottom.topAnchor.constraint(equalTo: view.topAnchor),
-         imageBottom.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-         imageBottom.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-         imageBottom.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ]
     }
 
     private func setupVerticalStackConstraints() -> [NSLayoutConstraint] {
         [ verticalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-          verticalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-          verticalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+          verticalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+          verticalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
           verticalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ]
     }
 
-
-
     private func setupImageLogoConstraints() -> [NSLayoutConstraint] {
         [imageLogo.heightAnchor.constraint(equalToConstant: 80)]
+    }
+
+    private func setupEmailTextFieldConstraints() -> [NSLayoutConstraint] {
+        [ 
+            emailTextField.heightAnchor.constraint(equalToConstant: 40)
+        ]
+    }
+
+    func setupStyle() {
+        setGradientBackground()
     }
 }
 
