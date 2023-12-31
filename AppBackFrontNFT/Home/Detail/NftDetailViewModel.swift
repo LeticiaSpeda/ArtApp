6abbdd8d7a519425658bda1b3f839fe47891b790
Leticia Speda <leticiaspeda@gmail.com>
2023-12-31 11:11:08 -0300

    feat: adicionado novas logicas para a celula de descricao

--- AppBackFrontNFT/Home/Detail/NftDetailViewModel.swift
+++ AppBackFrontNFT/Home/Detail/NftDetailViewModel.swift
@@ -4,20 +4,48 @@ protocol NftDetailViewModeling {
     var nft: Nft { get }
     var numberOfRowsInSection: Int { get }
     var nftImage: String { get }
+    var idNft: Int { get }
+    var nftTitleLabel: String { get }
+    var nftdescription: String { get }
+
+    func heightForRowAt(indexPath: IndexPath) -> CGFloat
 }
 
 final class NftDetailViewModel: NftDetailViewModeling {
     var nft: Nft
 
     var numberOfRowsInSection: Int {
-        return 1
+        return 2
     }
 
     var nftImage: String {
         return nft.nftImage ?? ""
     }
 
+    var idNft: Int {
+        return nft.nftID ?? 0
+    }
+
+    var nftTitleLabel: String {
+        return nft.nftNameImage ?? ""
+    }
+
+    var nftdescription: String {
+        return nft.nftDescription ?? ""
+    }
+
     init(nft: Nft) {
         self.nft = nft
+    }
+
+    func heightForRowAt(indexPath: IndexPath) -> CGFloat {
+        switch NftDetail(rawValue: indexPath.row) {
+        case .nftImage:
+            return 400
+        case .description:
+            return 250
+        default:
+            return 0
+        }
     }
 }
