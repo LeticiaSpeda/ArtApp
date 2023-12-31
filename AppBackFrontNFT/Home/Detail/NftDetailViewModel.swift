717dca747e2022900133ee9604d9c63b3ca81880
Leticia Speda <leticiaspeda@gmail.com>
2023-12-31 10:08:37 -0300

    feat: adicionado viewModel para cuidar da logica da tela de detalhes;

--- AppBackFrontNFT/Home/Detail/NftDetailViewModel.swift
+++ AppBackFrontNFT/Home/Detail/NftDetailViewModel.swift
@@ -1,11 +1,21 @@
 import UIKit
 
 protocol NftDetailViewModeling {
-    var nft: Nft { get set }
+    var nft: Nft { get }
+    var numberOfRowsInSection: Int { get }
+    var nftImage: String { get }
 }
 
 final class NftDetailViewModel: NftDetailViewModeling {
     var nft: Nft
+
+    var numberOfRowsInSection: Int {
+        return 1
+    }
+
+    var nftImage: String {
+        return nft.nftImage ?? ""
+    }
 
     init(nft: Nft) {
         self.nft = nft
