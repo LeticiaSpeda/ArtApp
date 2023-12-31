614e4078422114b06577b95626053dbf2e61be67
Leticia Speda <leticiaspeda@gmail.com>
2023-12-31 11:33:37 -0300

    refactor: retirado arquivo que nao pertencia a LoginViewModel e mudado para o arquivo de Extensions

--- AppBackFrontNFT/Login/LoginViewModel.swift
+++ AppBackFrontNFT/Login/LoginViewModel.swift
@@ -33,14 +33,4 @@ final class LoginViewModel {
     }
 }
 
-extension String {
-    func isValidEmail(email: String) -> Bool {
-       let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-       let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-       return emailPredicate.evaluate(with: email)
-   }
 
-    func isValidPassword(password: String) -> Bool {
-       return password.count >= 6
-   }
-}
