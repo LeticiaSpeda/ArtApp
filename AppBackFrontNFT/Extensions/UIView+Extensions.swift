23080444a844e6d8ad3280d66bdb818908bc8c8d
Leticia Speda <leticiaspeda@gmail.com>
2024-01-01 13:57:05 -0300

    feat: adicionado helper para arredondar a tableView

--- AppBackFrontNFT/Extensions/UIView+Extensions.swift
+++ AppBackFrontNFT/Extensions/UIView+Extensions.swift
@@ -4,4 +4,10 @@ extension UIView {
     func enableViewCode() {
         translatesAutoresizingMaskIntoConstraints = false
     }
+
+    func roundCorners(cornerRadiuns: Double,typeCorners: CACornerMask){
+        self.layer.cornerRadius = CGFloat(cornerRadiuns)
+        self.clipsToBounds = true
+        self.layer.maskedCorners = typeCorners
+    }
 }
