72e5a25879295e2b82363011cf755625b9ab3d9d
Leticia Speda <leticiaspeda@gmail.com>
2024-01-02 13:02:59 -0300

    feat: criado a tela com componentes que apresentara o wallet

--- AppBackFrontNFT/Features/Wallet/WalletViewController.swift
+++ AppBackFrontNFT/Features/Wallet/WalletViewController.swift
@@ -1,8 +1,30 @@
-//
-//  WalletViewController.swift
-//  AppBackFrontNFT
-//
-//  Created by Leticia Speda on 02/01/24.
-//
+import UIKit
 
-import Foundation
+final class WalletViewController: UIViewController, ViewCode {
+
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.separatorStyle = .none
+        table.backgroundColor = .backgroudColorMain
+        table.enableViewCode()
+        return table
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commonInit()
+    }
+
+    func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
