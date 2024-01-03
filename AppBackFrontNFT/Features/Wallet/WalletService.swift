c20198754b81d2ffd63f47278fa67acc95746037
Leticia Speda <leticiaspeda@gmail.com>
2024-01-03 09:33:24 -0300

    feat: adicionado service com as funcoes que chamam a api

--- AppBackFrontNFT/Features/Wallet/WalletService.swift
+++ AppBackFrontNFT/Features/Wallet/WalletService.swift
@@ -2,16 +2,16 @@ import Foundation
 import Alamofire
 
 protocol WalletServing: GenericService {
-    func getWalletFromJson(completion: @escaping completion<NFTData?>)
-    func getWallet(completion: @escaping completion<NFTData?>)
+    func getWalletFromJson(completion: @escaping completion<WalletData?>)
+    func getWallet(completion: @escaping completion<WalletData?>)
 }
 
 final class WalletService:  WalletServing {
-    func getWalletFromJson(completion: @escaping completion<NFTData?>) {
+    func getWalletFromJson(completion: @escaping completion<WalletData?>) {
         if let url = Bundle.main.url(forResource: "WalletData", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: url)
-                let nftData: NFTData = try JSONDecoder().decode(NFTData.self, from: data)
+                let nftData: WalletData = try JSONDecoder().decode(WalletData.self, from: data)
                 completion(nftData, nil)
             } catch {
                 completion(nil, Error.fileDecodingFaliled(name: "WalletData", error))
@@ -21,10 +21,10 @@ final class WalletService:  WalletServing {
         }
     }
 
-    func getWallet(completion: @escaping completion<NFTData?>) {
+    func getWallet(completion: @escaping completion<WalletData?>) {
         let url = "https://run.mocky.io/v3/ca84d863-7fdd-4105-b1a9-ef157b120d77"
 
-        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of: NFTData.self) { response in
+        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of: WalletData.self) { response in
 
             switch response.result {
             case .success(let sucess):
@@ -36,6 +36,4 @@ final class WalletService:  WalletServing {
             }
         }
     }
-
-
 }
