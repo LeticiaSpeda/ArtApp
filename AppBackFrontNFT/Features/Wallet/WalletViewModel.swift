14ec1fbb6955b3ec97f37f979dc8cfa5cc1804ac
Leticia Speda <leticiaspeda@gmail.com>
2024-01-03 09:35:15 -0300

    feat: criado viewModel para cuidar da logica

--- AppBackFrontNFT/Features/Wallet/WalletViewModel.swift
+++ AppBackFrontNFT/Features/Wallet/WalletViewModel.swift
@@ -1,8 +1,27 @@
-//
-//  WalletViewModel.swift
-//  AppBackFrontNFT
-//
-//  Created by Leticia Speda on 02/01/24.
-//
-
 import Foundation
+
+protocol WalletViewModeling {
+    func fetch(_ type: Typefeatch)
+}
+
+final class WalletViewModel: WalletViewModeling {
+
+    private let service: WalletServing
+
+    init(service: WalletServing) {
+        self.service = service   
+    }
+
+    func fetch(_ type: Typefeatch) {
+        switch type {
+        case .mock:
+            service.getWalletFromJson { result, failure in
+                print(result)
+            }
+        case .request:
+            service.getWallet { result, failure in
+                print(result)
+            }
+        }
+    }
+}
