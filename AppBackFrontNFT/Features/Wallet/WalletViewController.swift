d5a29c39d29860d84b20363f9de73b71721d1380
Leticia Speda <leticiaspeda@gmail.com>
2024-01-03 12:20:58 -0300

    feat: registrando a tableView e configurando a exibicao da celula

--- AppBackFrontNFT/Features/Wallet/WalletViewController.swift
+++ AppBackFrontNFT/Features/Wallet/WalletViewController.swift
@@ -7,12 +7,14 @@ final class WalletViewController: UIViewController, ViewCode {
         let table = UITableView()
         table.separatorStyle = .none
         table.backgroundColor = .backgroudColorMain
+        table.register(QuotationEthTableViewCell.self, forCellReuseIdentifier: QuotationEthTableViewCell.identifier)
         table.enableViewCode()
         return table
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.delegate = self
         viewModel.fetch(.request)
         commonInit()
     }
@@ -21,7 +23,7 @@ final class WalletViewController: UIViewController, ViewCode {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) { nil }
 
@@ -31,10 +33,41 @@ final class WalletViewController: UIViewController, ViewCode {
 
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
+}
+
+extension WalletViewController: WalletViewModelDelegate {
+    func sucess() {
+        DispatchQueue.main.async {
+            self.tableView.delegate = self
+            self.tableView.dataSource = self
+            self.tableView.reloadData()
+        }
+    }
+
+    func error() {
+        print(#function)
+    }
+}
+
+extension WalletViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: QuotationEthTableViewCell.identifier, for: indexPath) as? QuotationEthTableViewCell
+        cell?.setupCell(data: viewModel.loadCurrentQuotationEthreum(indexPath: indexPath))
+        return cell ?? UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return viewModel.heightForRowAt(indexPath: indexPath)
+    }
+
 }
