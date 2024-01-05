a6ba37611ffdcd40178fb7c1872557a8d9f70c0d
Leticia Speda <leticiaspeda@gmail.com>
2024-01-05 12:27:47 -0300

    feat: criado viewControlelr onde contem a table view com duas celulas na tela de perfil

--- AppBackFrontNFT/Features/Profile/ProfileViewController.swift
+++ AppBackFrontNFT/Features/Profile/ProfileViewController.swift
@@ -1,8 +1,58 @@
-//
-//  ProfileViewController.swift
-//  AppBackFrontNFT
-//
-//  Created by Leticia Speda on 05/01/24.
-//
+import UIKit
 
-import Foundation
+final class ProfileViewController: UIViewController, ViewCode {
+
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .backgroudDarkGray
+        table.separatorStyle = .none
+        table.delegate = self
+        table.dataSource = self
+        table.register(ProfileImageTableViewCell.self, forCellReuseIdentifier: ProfileImageTableViewCell.identifier)
+        table.register(ProfileTableViewCell.self, forCellReuseIdentifier: ProfileTableViewCell.identifier)
+        table.enableViewCode()
+        return table
+    }()
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commonInit()
+    }
+
+    func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ProfileImageTableViewCell.identifier, for: indexPath) as? ProfileImageTableViewCell
+
+            return cell ?? UITableViewCell()
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ProfileTableViewCell.identifier, for: indexPath) as? ProfileTableViewCell
+            return cell ?? UITableViewCell()
+        }
+    }
+
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 600
+//    }
+}
